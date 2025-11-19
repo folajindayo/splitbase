e969ddc9810697d9806e5280b64920b8de7101db
folajindayo <temidayofolajin@yahoo.com>
2025-11-19 13:08:40 +0100

    Add component index files for cleaner imports

---
+++ app/components/escrow/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Escrow Components
+ * Central export point for all escrow-related components
+ */
+
+export { default as CreateEscrowModal } from "./CreateEscrowModal";
+export { default as DepositFunds } from "./DepositFunds";
+export { default as DisputeModal } from "./DisputeModal";
+export { default as EscrowAnalyticsDashboard } from "./EscrowAnalyticsDashboard";
+export { default as EscrowCalculatorWidget } from "./EscrowCalculatorWidget";
+export { default as EscrowCard } from "./EscrowCard";
+export { default as EscrowNotes } from "./EscrowNotes";
+export { default as EscrowQuickStats } from "./EscrowQuickStats";
+export { default as EscrowRecommendations } from "./EscrowRecommendations";
+export { default as EscrowTemplates } from "./EscrowTemplates";
+export { default as EscrowTimeline } from "./EscrowTimeline";
+export { default as ShareEscrowModal } from "./ShareEscrowModal";
+
---
+++ app/components/ui/index.ts
@@ -0,0 +1,29 @@
+/**
+ * UI Components
+ * Central export point for all UI components
+ */
+
+export { default as Accordion } from "./Accordion";
+export { default as Alert } from "./Alert";
+export { default as Avatar } from "./Avatar";
+export { default as Badge } from "./Badge";
+export { default as Breadcrumb } from "./Breadcrumb";
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Checkbox } from "./Checkbox";
+export { default as Divider } from "./Divider";
+export { default as Dropdown } from "./Dropdown";
+export { default as Input } from "./Input";
+export { default as Modal } from "./Modal";
+export { default as Pagination } from "./Pagination";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as RadioGroup } from "./RadioGroup";
+export { default as Rating } from "./Rating";
+export { default as Select } from "./Select";
+export { default as Slider } from "./Slider";
+export { default as Spinner } from "./Spinner";
+export { default as Stepper } from "./Stepper";
+export { default as Table } from "./Table";
+export { default as Tabs } from "./Tabs";
+export { default as Tooltip } from "./Tooltip";
+
