137022773eb1919c46fb3deada2a2371ec791ccb
folajindayo <temidayofolajin@yahoo.com>
2025-11-19 22:14:27 +0100

    Add UI components barrel exports for clean imports

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,29 +1,10 @@
 /**
- * UI Components
- * Central export point for all UI components
+ * UI components barrel exports
  */
 
-export { default as Accordion } from "./Accordion";
-export { default as Alert } from "./Alert";
-export { default as Avatar } from "./Avatar";
-export { default as Badge } from "./Badge";
-export { default as Breadcrumb } from "./Breadcrumb";
-export { default as Button } from "./Button";
-export { default as Card } from "./Card";
-export { default as Checkbox } from "./Checkbox";
-export { default as Divider } from "./Divider";
-export { default as Dropdown } from "./Dropdown";
-export { default as Input } from "./Input";
-export { default as Modal } from "./Modal";
-export { default as Pagination } from "./Pagination";
-export { default as ProgressBar } from "./ProgressBar";
-export { default as RadioGroup } from "./RadioGroup";
-export { default as Rating } from "./Rating";
-export { default as Select } from "./Select";
-export { default as Slider } from "./Slider";
-export { default as Spinner } from "./Spinner";
-export { default as Stepper } from "./Stepper";
-export { default as Table } from "./Table";
-export { default as Tabs } from "./Tabs";
-export { default as Tooltip } from "./Tooltip";
-
+export * from "./Button";
+export * from "./Input";
+export * from "./ErrorBoundary";
+export * from "./Toast";
+export * from "./Tabs";
+export * from "./Accordion";
