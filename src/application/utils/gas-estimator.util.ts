bcc8a67c6e5a23321f7ca6b631cdf6980fdc5899
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 09:07:40 +0100

    feat(utils): add gas estimator utility

---
+++ src/application/utils/gas-estimator.util.ts
@@ -0,0 +1,27 @@
+/**
+ * Gas Estimator Utility
+ */
+
+export class GasEstimator {
+  private static readonly BASE_GAS = 21000n;
+  private static readonly PER_RECIPIENT_GAS = 50000n;
+
+  static estimateSplitGas(recipientCount: number): bigint {
+    return this.BASE_GAS + this.PER_RECIPIENT_GAS * BigInt(recipientCount);
+  }
+
+  static calculateGasCost(gasAmount: bigint, gasPrice: bigint): bigint {
+    return gasAmount * gasPrice;
+  }
+
+  static addGasBuffer(gasAmount: bigint, bufferPercent: number = 20): bigint {
+    const buffer = (gasAmount * BigInt(bufferPercent)) / 100n;
+    return gasAmount + buffer;
+  }
+
+  static formatGasPrice(gasPrice: bigint): string {
+    const gwei = Number(gasPrice) / 1e9;
+    return `${gwei.toFixed(2)} Gwei`;
+  }
+}
+
