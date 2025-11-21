4465dbfd95809ea3761a0514055405852ad48094
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 08:29:01 +0100

    feat(splitbase): add metadata constants

---
+++ lib/constants/metadata.ts
@@ -0,0 +1,12 @@
+/**
+ * Metadata Constants
+ */
+
+export const METADATA = {
+  APP_NAME: 'Splitbase',
+  APP_DESCRIPTION: 'Split payments made simple',
+  APP_URL: 'https://splitbase.app',
+  TWITTER_HANDLE: '@splitbase',
+  OG_IMAGE: '/og-image.png',
+} as const;
+
