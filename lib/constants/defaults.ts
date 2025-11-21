c6b52f550643f547ff94636e5114af07682c3180
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 08:08:54 +0100

    feat(splitbase): add default constants

---
+++ lib/constants/defaults.ts
@@ -0,0 +1,12 @@
+/**
+ * Default Constants
+ */
+
+export const DEFAULTS = {
+  MIN_RECIPIENTS: 2,
+  MAX_RECIPIENTS: 20,
+  DEFAULT_SHARE: 50,
+  FEE_PERCENTAGE: 1,
+  GAS_BUFFER: 1.2,
+} as const;
+
