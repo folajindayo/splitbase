10e08d70b7e92d96c7941832fb419b789d1c95d4
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 13:15:05 +0100

    feat(lib): add application constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+/**
+ * Application-wide Constants
+ */
+
+export const APP_NAME = 'SplitBase';
+export const APP_DESCRIPTION = 'Split payments easily on-chain';
+export const APP_URL = 'https://splitbase.app';
+
+export const MAX_RECIPIENTS = 100;
+export const MIN_SPLIT_AMOUNT = '0.000001';
+export const PLATFORM_FEE_PERCENTAGE = 0.5;
+
+export const GAS_BUFFER_MULTIPLIER = 1.2;
+export const CONFIRMATION_BLOCKS = 3;
+
