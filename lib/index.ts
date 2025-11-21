9484cfcaf401dca667ba1d2cdbafd35fd80ee77b
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 08:22:26 +0100

    feat(splitbase): add main library exports

---
+++ lib/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Main Library Exports
+ */
+
+// Services
+export * from './services/split.service';
+export * from './services/escrow.service';
+
+// Utilities
+export * from './utils/split-calculator';
+export * from './utils/share-calculator';
+export * from './utils/decimal.utils';
+
+// Constants
+export * from './constants/split-limits';
+export * from './constants/payment.config';
+
+// Types
+export * from './types/split.types';
+export * from './types/recipient.types';
+
